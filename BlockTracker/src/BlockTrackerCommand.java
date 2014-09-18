8c873073176544c6b1659072a1b3fc4bb2fc087b
Geistes <Geistes@hotmail.com>
2014-09-18 09:35:26 +0800

    Beginning command interface

---
+++ BlockTracker/src/BlockTrackerCommand.java
@@ -0,0 +1,30 @@
+import net.minecraft.server.MinecraftServer;
+
+public class BlockTrackerCommand extends z {
+
+	public boolean a(ae var1) {
+		return MinecraftServer.M().S() || super.a(var1);
+	}
+
+	public String c() {
+		return "bt";
+	}
+
+	public int a() {
+		return 1;
+	}
+
+	public String c(ae var1) {
+		return "usage: bt usage";
+	}
+
+	public void a(ae var1, String[] var2) {
+		String test = "test";
+		var1.a(new hz("Response 1", new Object[] { test }));
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		return 0;
+	}
+}
