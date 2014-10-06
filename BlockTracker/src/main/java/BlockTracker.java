75de1ea049c5591748b23b5abc93f97fd9d17c00
Vinnie <vmatronia@gmail.com>
2014-10-06 14:40:20 -0400

    Queue block actions
    Use an ExecutorService to handle writing to the database. This will
    greatly reduce lag when logging and should keep from overloading the
    database.

--- BlockTracker/src/main/java/BlockTracker.java
+++ BlockTracker/src/main/java/BlockTracker.java
@@ -1,16 +1,15 @@
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
 public class BlockTracker extends Thread {
-
 	public static final Logger logger = LogManager.getLogger();
+    public static final ExecutorService queue = Executors.newFixedThreadPool(2);
 	public static String host;
 	public static String database;
 	public static String dbuser;
@@ -45,36 +44,43 @@ public class BlockTracker extends Thread {
 	//Called when a player breaks a block
 	public void BlockBreakEvent(aqu world, dt BBcoords, bec blocktype, ahd player) {
 		if (Track) {
-			String BlockType = String.valueOf(blocktype);
+			final String BlockType = String.valueOf(blocktype);
 
 			//Converts the dt object to X, Y, and Z variables
-			int X = BBcoords.n();
-			int Y = BBcoords.o();
-			int Z = BBcoords.p();
+			final int X = BBcoords.n();
+			final int Y = BBcoords.o();
+			final int Z = BBcoords.p();
 			
 			//Isolates the playername from the ahd object
-			String Player = player.d_();
+			final String Player = player.d_();
 
 			//Insert to DB
-			BlockTrackerSQL.insertBlockBreak(Player, X, Y, Z, getTime(), BlockType);
+            queue.execute(new Runnable() {
+                public void run() {
+                    BlockTrackerSQL.insertBlockBreak(Player, X, Y, Z, getTime(), BlockType);
+                }
+            });
 		}
 	}
 
 	//Called when a player places a block
 	public void BlockPlaceEvent(aqu world, dt BPcoords, bec blocktype, xm entitylivingbase, amj itemstack) {
 		if (Track) {
-			String BlockType = String.valueOf(blocktype);
+			final String BlockType = String.valueOf(blocktype);
 
 			//Converts the dt object to X, Y, and Z variables
-			int X = BPcoords.n();
-			int Y = BPcoords.o();
-			int Z = BPcoords.p();
+			final int X = BPcoords.n();
+			final int Y = BPcoords.o();
+			final int Z = BPcoords.p();
 			
 			//Isolates the playername from the xm object
-			String Player = entitylivingbase.d_();
-			
+			final String Player = entitylivingbase.d_();
 
-			BlockTrackerSQL.insertBlockPlace(Player, X, Y, Z, getTime(), BlockType);
+            queue.execute(new Runnable() {
+                public void run() {
+                    BlockTrackerSQL.insertBlockPlace(Player, X, Y, Z, getTime(), BlockType);
+                }
+            });
 		}
 
 	}
@@ -83,8 +89,6 @@ public class BlockTracker extends Thread {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
 		return dateFormat.format(date);
-
 	}
-
 }
 
